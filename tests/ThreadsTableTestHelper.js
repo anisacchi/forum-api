455aa1da84a6860d3d7e9ae7fe63116ba3574431
anisa2472 <anisa.psari24@gmail.com>
2023-03-13 08:09:03 +0700

    :eslint

--- tests/ThreadsTableTestHelper.js
+++ tests/ThreadsTableTestHelper.js
@@ -2,30 +2,30 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const ThreadsTableTestHelper = {
-	async addThread({
-		id = 'thread-123', title = 'Title', body = 'This is the thread body', owner = 'user-123'
-	}) {
-		const query = {
-			text: 'INSERT INTO threads VALUES($1, $2, $3, $4)',
-			values: [id, title, body, owner]
-		};
+  async addThread({
+    id = 'thread-123', title = 'Title', body = 'This is the thread body', owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4)',
+      values: [id, title, body, owner],
+    };
 
-		await pool.query(query);
-	},
+    await pool.query(query);
+  },
 
-	async findThreadById(id) {
-		const query = {
-			text: 'SELECT * FROM threads WHERE id = $1',
-			values: [id]
-		};
+  async findThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
 
-		const result = await pool.query(query);
-		return result.rows;
-	},
+    const result = await pool.query(query);
+    return result.rows;
+  },
 
-	async cleanTable() {
-		await pool.query('DELETE FROM threads WHERE 1=1')
-	}
-}
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
 
 module.exports = ThreadsTableTestHelper;
