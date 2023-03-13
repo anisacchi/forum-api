4bead2b902ac8584f5d52d6c36f9cc37185cfcca
anisa2472 <anisa.psari24@gmail.com>
2023-03-13 16:43:23 +0700

    :prepare comments

---
+++ tests/CommentsTableTestHelper.js
@@ -0,0 +1,36 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const CommentsTableTestHelper = {
+  async addComment({
+    id = 'comment-123',
+    content = 'This is a comment',
+    date = '1600-01-01T00:00:00.000Z',
+    owner = 'user-123',
+    threadId = 'thread-123',
+    isDelete = false,
+  }) {
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6)',
+      values: [id, content, date, owner, threadId, isDelete],
+    };
+
+    await pool.query(query);
+  },
+
+  async findCommentById(id) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM comments WHERE 1=1');
+  },
+};
+
+module.exports = CommentsTableTestHelper;
