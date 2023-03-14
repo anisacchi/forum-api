dc69f187e60ed93542138395d7b467594d053603
anisa2472 <anisa.psari24@gmail.com>
2023-03-14 08:57:43 +0700

    :add CommentRepository interface

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,7 @@
+class CommentRepository {
+  async addComment(credentialId, comment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
