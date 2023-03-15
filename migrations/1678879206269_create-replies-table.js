67f2658fe2adb58ceefed619d0ea6fae9574839d
anisa2472 <anisa.psari24@gmail.com>
2023-03-15 18:50:52 +0700

    :prepare replies

---
+++ migrations/1678879206269_create-replies-table.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'comments',
+      onDelete: 'CASCADE',
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      noNull: true,
+      default: false,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('replies');
+};
