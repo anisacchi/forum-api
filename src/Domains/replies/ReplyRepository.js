0eb66c188d82c45265303c768e175095ef2fd400
anisa2472 <anisa.psari24@gmail.com>
2023-03-17 06:00:33 +0700

    :update getReplyById, verifyOwner, and deleteReply function

--- src/Domains/replies/ReplyRepository.js
+++ src/Domains/replies/ReplyRepository.js
@@ -2,6 +2,18 @@ class ReplyRepository {
   async addReply(credentialId, commentId, reply) {
     throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async getReplyById(id) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyOwner(userId, replyId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReply(id) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ReplyRepository;
