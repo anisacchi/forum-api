8bda08bb9c75f48ba62ec6c6c9cacbda421cac97
anisa2472 <anisa.psari24@gmail.com>
2023-03-17 08:27:56 +0700

    :add getReplyById, verifyOwner, and deleteReply function

--- src/Infrastructures/repository/ReplyRepositoryPostgres.js
+++ src/Infrastructures/repository/ReplyRepositoryPostgres.js
@@ -1,5 +1,7 @@
 const AddedReply = require('../../Domains/replies/entities/AddedReply');
 const ReplyRepository = require('../../Domains/replies/ReplyRepository');
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError');
 
 class ReplyRepositoryPostgres extends ReplyRepository {
   constructor(pool, idGenerator) {
@@ -20,6 +22,40 @@ class ReplyRepositoryPostgres extends ReplyRepository {
     const result = await this._pool.query(query);
 
     return new AddedReply({ ...result.rows[0] });
+  }
+
+  async getReplyById(replyId) {
+    const query = {
+      text: 'SELECT * FROM replies WHERE id = $1',
+      values: [replyId],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Reply not found.');
+    }
+
+    return result.rows[0];
+  }
+
+  async verifyOwner(credentialId, replyId) {
+    const query = {
+      text: 'SELECT * FROM replies WHERE id = $1 AND owner = $2',
+      values: [replyId, credentialId],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthorizationError('Failed to delete reply. Only the reply owner can delete it.');
+    }
+  }
+
+  async deleteReply(replyId) {
+    const query = {
+      text: 'UPDATE replies SET is_delete = true WHERE id = $1',
+      values: [replyId],
+    };
+    await this._pool.query(query);
   }
 }
 
--- src/Infrastructures/repository/_test/ReplyRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/ReplyRepositoryPostgres.test.js
@@ -65,4 +65,79 @@ describe('ReplyRepositoryPostgres', () => {
       );
     });
   });
+
+  describe('getReplyById function', () => {
+    it('should throw error when reply does not exist', async () => {
+      // Arrange
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool);
+
+      // Action and Assert
+      await expect(replyRepositoryPostgres.getReplyById('reply-123')).rejects.toThrowError('Reply not found.');
+    });
+
+    it('should return reply correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123' });
+
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool);
+
+      // Action
+      const reply = await replyRepositoryPostgres.getReplyById('reply-123');
+
+      // Assert
+      expect(reply.id).toEqual('reply-123');
+    });
+  });
+
+  describe('verifyOwner function', () => {
+    it('should throw error when reply and owner do not match', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123', owner: 'user-123' });
+
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool);
+
+      // Action and Assert
+      await expect(replyRepositoryPostgres.verifyOwner('user-1', 'reply-123'))
+        .rejects.toThrowError('Failed to delete reply. Only the reply owner can delete it.');
+    });
+
+    it('should not throw error when reply and owner match', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123', owner: 'user-123' });
+
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool);
+
+      // Action and Assert
+      await expect(replyRepositoryPostgres.verifyOwner('user-123', 'reply-123'))
+        .resolves.not.toThrowError('Failed to delete reply. Only the reply owner can delete it.');
+    });
+  });
+
+  describe('deleteReply function', () => {
+    it('should delete reply from database', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123', threadId: 'thread-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123', owner: 'user-123', commentId: 'comment-123' });
+
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool);
+
+      // Action
+      await replyRepositoryPostgres.deleteReply('reply-123');
+
+      // Assert
+      const deletedReply = await RepliesTableTestHelper.findReplyById('reply-123');
+      expect(deletedReply[0].is_delete).toBeTruthy();
+    });
+  });
 });
